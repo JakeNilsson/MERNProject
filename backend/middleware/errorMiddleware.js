a652aea263d45db393c41289be6ed91daa3df4a2
Jake Nilsson <JakeANilsson@gmail.com>
2024-01-27 15:17:53 -0600

    middleware routing and error messaging

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,23 @@
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
+
+    //Check for mongoose bad ID for objects
+    if(err.name === 'CaseError' && err.kind === 'ObjectId') {
+        message = `Resource not found`;
+        statusCode = 404;
+    }
+
+    res.status(statusCode).json({
+         message,
+         stack: provess.env.NODE_ENV === 'production' ? 'thicc stacc' : err.stack,
+    });
+};
+
+export {notFound, errorHandler};
