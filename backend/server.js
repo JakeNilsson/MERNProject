6d52943b835e9fa2f96e584b619cc7cffd4a0fbe
Jake Nilsson <JakeANilsson@gmail.com>
2024-02-07 20:07:41 -0500

    Solidify user authentication, password and email checking

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ connectDB();
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
