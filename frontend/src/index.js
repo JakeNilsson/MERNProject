8811098df68bbb8629a5ded9d96b10eab1a63dee
Jake Nilsson <JakeANilsson@gmail.com>
2024-02-16 01:26:12 -0500

    correctly route to placeorder page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,7 +33,8 @@ const router = createBrowserRouter(
       
 
       <Route path='' element={<PrivateRoute />}>
-        <Route path='/shipping' element={<ShippingScreen />} /> 
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} /> 
       </Route>
     </Route>
   )
