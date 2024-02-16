10d2541a40b459ae4243ebdaa9d6b12a9bc2ac5b
Jake Nilsson <JakeANilsson@gmail.com>
2024-02-16 17:16:11 -0500

    implement full UI for order placements

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,7 +1,7 @@
 import {apiSlice} from './apiSlice';
 import { ORDERS_URL } from '../constants';
 
-export const ordersApiSlice = apiSlice.injectionEndpoints({
+export const ordersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         createOrder: builder.mutation({
             query: (order) => ({
