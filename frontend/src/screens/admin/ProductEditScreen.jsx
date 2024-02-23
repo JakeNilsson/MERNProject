b53b6dc435f1388a4c27f4741ae19fae862cc19a
Jake Nilsson <JakeANilsson@gmail.com>
2024-02-23 00:39:42 -0500

    implement upload and change image capability

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -5,7 +5,11 @@ import Message from "../../components/Message"
 import Loader from "../../components/Loader"
 import FormContainer from "../../components/FormContainer"
 import {toast} from "react-toastify"
-import {useUpdateProductMutation, useGetProductDetailsQuery} from "../../slices/productsApiSlice"
+import {
+    useUpdateProductMutation, 
+    useGetProductDetailsQuery,
+    useUploadProductImageMutation,
+} from "../../slices/productsApiSlice"
 
 
 const ProductEditScreen = () => {
@@ -22,6 +26,8 @@ const ProductEditScreen = () => {
     const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId);
 
     const [updateProduct, {isLoading: loadingUpdate}] = useUpdateProductMutation();
+
+    const [uploadProductImage, {isLoading: loadingUpload}] = useUploadProductImageMutation();
 
     const navigate = useNavigate();
 
@@ -56,6 +62,18 @@ const ProductEditScreen = () => {
         } else {
             toast.success('Product updated');
             navigate('/admin/productlist');
+        }
+    };
+
+    const uploadFileHandler = async (e) => {
+        const formData = new FormData();
+        formData.append('image', e.target.files[0]);
+        try {
+            const res = await uploadProductImage(formData).unwrap();
+            toast.success(res.message);
+            setImage(res.image);
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
         }
     }
 
@@ -94,7 +112,13 @@ const ProductEditScreen = () => {
                             </Form.Control>
                     </Form.Group>
 
-                    {/* IMAGE INPUT PLACEHOLDER */}
+                    <Form.Group controlId='image' className='my-2'>
+                        <Form.Label>Image</Form.Label>
+                        <Form.Control type="text" placeholder='Enter image url' 
+                        value={image} onChange={(e) => setImage}></Form.Control>
+
+                        <Form.Control type='file' label='Choose file' onChange={uploadFileHandler}></Form.Control>
+                    </Form.Group>
 
                     <Form.Group controlId='brand' className='my-2'>
                         <Form.Label>Brand</Form.Label>
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL } from "../constants";
+import { PRODUCTS_URL, UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -32,10 +32,18 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),
             invalidateTags: ['Products'],
+        }),
+
+        uploadProductImage: builder.mutation({
+            query: (data) => ({
+                url: `${UPLOAD_URL}`,
+                method: 'POST',
+                body: data,
+            })
         })
     }),
 });
 
 export const { useGetProductsQuery, useGetProductDetailsQuery,
-useCreateProductMutation, useUpdateProductMutation } = 
+useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation } = 
 productsApiSlice;
