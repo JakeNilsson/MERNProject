5260e87d82acf4c2c39c97c410a31ebbb40b94d7
Jake Nilsson <JakeANilsson@gmail.com>
2024-03-18 15:29:44 -0400

    brought in paginate component and fixed admin productlist bug

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import Product from "../components/Product";
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Paginate from '../components/Paginate';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
@@ -23,6 +24,9 @@ const HomeScreen = () => {
                   </Col>
               ))}
           </Row>
+          <Paginate
+            pages={data.pages}
+            page={data.page} />
         </>)}
     </>
   );
