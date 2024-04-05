be86d6fce3051f75c943f4762ff0763f20f0a53c
jds18b <jds18b@fsu.edu>
2024-04-05 19:17:38 -0400

    Removed two instances of commented-out code code smell: one in index.js and one in constants.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,6 @@ import {PayPalScriptProvider} from '@paypal/react-paypal-js'
 import {HelmetProvider} from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import store from './store';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
