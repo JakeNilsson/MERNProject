c108e62f686c00f77f0470a16bdefb1201dd8af2
BeRaidin <braydenclewis@gmail.com>
2024-04-19 15:03:18 -0400

    Search dropdown displays unique categories

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,41 +1,58 @@
 import { useState } from "react";
-import {Form, Button} from 'react-bootstrap';
-import {useParams, useNavigate} from 'react-router-dom';
+import { Form, Button } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { useGetProductCategoriesQuery } from "../slices/productsApiSlice";
+import Loader from "./Loader";
+import Message from "./Message";
 
 const SearchBox = () => {
-    const navigate = useNavigate();
-    const {keyword: urlKeyword} = useParams();
-    const [keyword, setKeyword] = useState(urlKeyword || '');
+  const navigate = useNavigate();
+  const { keyword: urlKeyword } = useParams();
+  const [keyword, setKeyword] = useState(urlKeyword || "");
+  const { data: categories, isLoading, error } = useGetProductCategoriesQuery();
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        if (keyword.trim()) {
-            setKeyword('');
-            navigate(`/search/${keyword}`);
-        } else {
-            navigate('/');
-        }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      setKeyword("");
+      navigate(`/search/${keyword}`);
+    } else {
+      navigate("/");
     }
-
+  };
+  if (isLoading) {
+    return <Loader />;
+  } else if (error) {
+    return <Message variant="danger">{error}</Message>;
+  } else {
     return (
-        <Form onSubmit={submitHandler} className='d-flex'>
-            <Form.Control
-                type='text'
-                name='q'
-                onChange={(e) => setKeyword(e.target.value)}
-                value={keyword}
-                placeholder='Search Products...'
-                className='mr-sm-2 ml-sm-5'>
+      <Form onSubmit={submitHandler} className="d-flex">
+        <Form.Control
+          type="text"
+          name="q"
+          onChange={(e) => setKeyword(e.target.value)}
+          value={keyword}
+          placeholder="Search Products..."
+          className="mr-sm-2 ml-sm-5"
+        ></Form.Control>
+        <Dropdown as={ButtonGroup} className="mx-2" autoClose="outside">
+          <Button type="submit" variant="outline-light">
+            Search
+          </Button>
+          <Dropdown.Toggle split variant="outline-light" />
+          <Dropdown.Menu>
+            <Dropdown.ItemText>Categories</Dropdown.ItemText>
+            <Dropdown.Divider />
+            {categories.map((category) => (
+              <Dropdown.Item>{category}</Dropdown.Item>
+            ))}{" "}
+          </Dropdown.Menu>
+        </Dropdown>
+      </Form>
+    );
+  }
+};
 
-                </Form.Control>
-                <Button
-                    type='submit'
-                    variant='outline-light'
-                    className='p-2 mx-2'>
-                        Search
-                </Button>
-        </Form>
-    )
-}
-
-export default SearchBox
+export default SearchBox;
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -2,82 +2,89 @@ import { PRODUCTS_URL, UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getProducts: builder.query({
-            query: ({keyword, pageNumber}) => ({
-                url: PRODUCTS_URL,
-                params: {
-                    keyword,
-                    pageNumber,
-                }
-            }),
-            keepUnusedDataFor: 5,
-            providesTags: ['Products'],
-        }),
-        getProductDetails: builder.query({
-            query: (productId) => ({
-                url: `${PRODUCTS_URL}/${productId}`,
-            }),
-            keepUnusedDataFor: 5,
-        }),
-
-        createProduct: builder.mutation({
-            query: () => ({
-                url: PRODUCTS_URL,
-                method: 'POST',
-            }),
-            invalidateTags: ['Product'],
-        }),
-
-        updateProduct: builder.mutation({
-            query: (data) => ({
-                url: `${PRODUCTS_URL}/${data.productId}`,
-                method: 'PUT',
-                body: data,
-            }),
-            invalidateTags: ['Products'],
-        }),
-
-        uploadProductImage: builder.mutation({
-            query: (data) => ({
-                url: `${UPLOAD_URL}`,
-                method: 'POST',
-                body: data,
-            })
-        }),
-
-        deleteProduct: builder.mutation({
-            query: (productId) => ({
-                url: `${PRODUCTS_URL}/${productId}`,
-                method: 'DELETE',
-            }),
-        }),
-
-        createReview: builder.mutation({
-            query: (data) => ({
-                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
-                method: 'POST',
-                body: data,
-            }),
-            invalidateTags: ['Product'],
-        }),
-
-        getTopProducts: builder.query({
-            query: () => ({
-                url: `${PRODUCTS_URL}/top`,
-            }),
-            keepUnusedDataFor: 5,
-        })
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: ({ keyword, pageNumber }) => ({
+        url: PRODUCTS_URL,
+        params: {
+          keyword,
+          pageNumber,
+        },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ["Products"],
     }),
+    getProductCategories: builder.query({
+      query: () => ({
+        url: `${PRODUCTS_URL}/categories`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: "POST",
+      }),
+      invalidateTags: ["Product"],
+    }),
+
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidateTags: ["Products"],
+    }),
+
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `${UPLOAD_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: "DELETE",
+      }),
+    }),
+
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: "POST",
+        body: data,
+      }),
+      invalidateTags: ["Product"],
+    }),
+
+    getTopProducts: builder.query({
+      query: () => ({
+        url: `${PRODUCTS_URL}/top`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
 });
 
-export const { 
-    useGetProductsQuery, 
-    useGetProductDetailsQuery,
-    useCreateProductMutation, 
-    useUpdateProductMutation, 
-    useUploadProductImageMutation,
-    useDeleteProductMutation, 
-    useCreateReviewMutation,
-    useGetTopProductsQuery,
+export const {
+  useGetProductsQuery,
+  useGetProductCategoriesQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+  useDeleteProductMutation,
+  useCreateReviewMutation,
+  useGetTopProductsQuery,
 } = productsApiSlice;
