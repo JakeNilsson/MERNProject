7ca1df99e4e9878450596be8b07d5ab07a087cd3
jds18b <jds18b@fsu.edu>
2024-04-24 22:20:18 -0400

    Added api slice for saved items

---
+++ frontend/src/slices/saveSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("saved")
+? JSON.parse(localStorage.getItem("saved"))
+: {savedItems : []};
+
+const saveSlice = createSlice({
+    name: "saved",
+    initialState,
+    reducers: {
+        addToSavedItems: (state, action) => {
+            const item = action.payload;
+
+            const existItem = state.savedItems.find((x) => x._id === item._id);
+
+            if (existItem) {
+                state.savedItems = state.savedItems.map((x) =>
+                x._id === existItem._id ? item : x
+            );
+            } else {
+                state.savedItems = [...state.savedItems, item];
+            }
+
+            localStorage.setItem('saved', JSON.stringify(state));
+        },
+        removeFromSavedItems: (state, action) => {
+            state.savedItems = state.savedItems.filter((x) => x._id !== action.payload);
+      
+            localStorage.setItem('saved', JSON.stringify(state));
+          },
+    },
+});
+
+export const {addToSavedItems} = saveSlice.actions;
+
+export default saveSlice.reducer;
