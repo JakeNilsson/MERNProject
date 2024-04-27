b217ae755c6caa1f65d90bd32540e7fc956fbda9
Jake Nilsson <JakeANilsson@gmail.com>
2024-04-27 14:55:41 -0400

    final code cleaning and removing unused dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-//import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -65,7 +65,7 @@ const router = createBrowserRouter(
   )
 )
 
-const root = ReactDOM.createRoot(document.getElementById('root')); //here - idk why I left this comment. ;n;
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
       <HelmetProvider>
